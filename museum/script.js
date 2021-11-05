d2f5341029d9004e82699a8ffbda65e03b24b4d9
Olga <olga.dashuk21@gmail.com>
2021-11-05 18:08:53 +0300

    feat: add carousel to welcome section

--- museum/script.js
+++ museum/script.js
@@ -36,3 +36,81 @@ function plusSenior () {
 
 const basicAmount = document.querySelector(".basic__amount");
 const seniorAmount = document.querySelector(".senior__amount");
+
+// Image carousel
+
+let imageSlideIndex = 1;
+showImageSlides(imageSlideIndex);
+
+// Next/previous controls
+function plusImageSlides(n) {
+  imageSlideIndex = imageSlideIndex + n;
+  showImageSlides(imageSlideIndex);
+}
+
+// Thumbnail image controls
+function currentImageSlide(n) {
+  imageSlideIndex = n
+  showImageSlides(imageSlideIndex);
+}
+
+function showImageSlides(n) {
+  let imageSlides = document.getElementsByClassName("welcome__carousel-image");
+  let imageDots = document.getElementsByClassName("welcome__control");
+  let currentImage = document.querySelector(".welcome__counter");
+ 
+  if (n > imageSlides.length) {imageSlideIndex = 1}
+  if (n < 1) {imageSlideIndex = imageSlides.length}
+
+  for (i = 0; i < imageSlides.length; i++) {
+    imageSlides[i].style.display = "none";
+  }
+  for (i = 0; i < imageDots.length; i++) {
+    imageDots[i].className = imageDots[i].className.replace(" welcome__control--active", "");
+  }
+
+  imageSlides[imageSlideIndex-1].style.display = "block";
+  imageDots[imageSlideIndex-1].className += " welcome__control--active";
+  currentImage.innerHTML = "0" + imageSlideIndex;
+}
+
+
+// Video carousel
+
+let videoSlideIndex = 1;
+showVideoMain(videoSlideIndex);
+
+// Next/previous controls
+function plusVideoSlides(n) {
+  videoSlideIndex = videoSlideIndex + n;
+  showVideoMain(videoSlideIndex);
+}
+
+// // Thumbnail video controls
+function currentVideoSlide(n) {
+  videoSlideIndex = n;
+  showVideoMain(videoSlideIndex);
+}
+
+function showVideoMain(n) {
+//   let videoMain = document.getElementsByClassName("video__carousel-main");
+  let videoDots = document.getElementsByClassName("video__circle");
+  console.log(videoDots);
+  
+//   if (n > videoMain.length) {videoSlideIndex = 1}
+//   if (n < 1) {videoSlideIndex = videoMain.length}
+
+//   for (i = 0; i < imageSlides.length; i++) {
+//     imageSlides[i].style.display = "none";
+//   }
+  for (i = 0; i < videoDots.length; i++) {
+    videoDots[i].className = videoDots[i].className.replace(" video__circle--active", "");
+  }
+
+//   imageSlides[imageSlideIndex-1].style.display = "block";
+  videoDots[videoSlideIndex-1].className += " video__circle--active";
+}
+
+// function showVideoSlides (n) {
+
+// }
